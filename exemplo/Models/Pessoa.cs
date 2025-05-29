b6f75fcba8edf5e8b8a1a1746606c732fd103dbf
Samuel <sam1219barbosa@gmail.com>
2025-05-29 05:26:12 -0300

    Iniciando o projeto

---
+++ exemplo/Models/Pessoa.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace exemplo.Models
+{
+    public class Pessoa
+    {
+        [Display (Name = "Código do Cliente")]
+
+        public int PessoaId
+        {
+            /*o get é um metodo que retorna um valor atual de um atributo, o set permite definer ou modificar o valor do atributo*/
+            get; set;
+        }
+        [Display(Name = "Nome do Cliente")]
+        /*a interrogação informa que o campo esta null*/
+        public string? Nome
+        {
+            get; set;
+        }
+        [Display(Name = "Email do Cliente")]
+        public string? Email
+        {
+
+            get; set;
+        }
+    }
+}
